0e886368c8523f25b3d08e4bd6909e346f4e0fb6
Lationcesanjay <91967359+Lationcesanjay@users.noreply.github.com>
2024-12-09 18:57:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/home'
+  export default function App() {
+  return (
+    <>
+    <div style={{display:"flex" , justifyContent:"space-evenly"}}>
+  <div><Home/></div>
+       
+      
+    </div>
+    </>
+  )
+}
